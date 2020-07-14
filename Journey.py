4039699da79b5415491de9d0d9e8f019c9f9bcaf
ASGithubaccount <68076764+ASGithubaccount@users.noreply.github.com>
2020-07-14 21:45:18 +0200

    Update Journey.py

--- Journey.py
+++ Journey.py
@@ -8,4 +8,44 @@ try:
 except Exception as ex:
     print(ex)
 
-print('asdasda')
+# Eveything is an object
+type(1)
+type("String")
+
+# Variable assignement
+variable = 'assigned value'
+print(variable)
+
+# Data types
+a = 3
+b = 7.3
+c = "String"
+d = True
+e = None
+
+# Expressions, statements, literals
+expressions = "represents_value" 
+statements = "do_something"
+literals = ['numeric', 'string', 'boolean', 'special', 'collections']
+
+# Typecasting
+x = 3
+y = 'string'
+str(x) + y
+
+# Arithmetic operators (+, -, /, //, *, **, %)
+x * y
+
+# Arithmetic assignement operators (+=, -=, /=, //=, *=, **=, %=)
+
+# Arithmetic operation precedence (PEMDAS)
+
+# Basic comparison operators (<, >, <=, >=, ==, !=)
+
+# Logical operators (not, and, or)
+
+# Implicit boolean typecasting (Truthy and Falsy values)
+
+# Basic control flow (if, elif, else)
+
+# Grouping (indentation, tabs, spaces)
